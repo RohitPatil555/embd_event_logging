9a6e6dd48815d7f8c584903b3d3d591dc691f7c2
Rohit Patil <rohit.patil.wrc.555@gmail.com>
2025-10-26 09:46:39 +0000

    Title: Fixed CI and removed seperate example script Type: Fix Component: CI Detail:     - Make single script for example and CI     - Make default UT build for CI.

--- test_ci.sh
+++ test_ci.sh
@@ -5,37 +5,93 @@ set -e
 EXEC_DIR=$PWD
 BUILD_DIR=build
 COPY_ARTIFACTS=false
+EXAMPLE_BUILD=false
 
+function library_ut_test()
+{
+    # clean build directory
+    rm -rf ${BUILD_DIR}
+    mkdir ${BUILD_DIR}
+    cd ${BUILD_DIR}
 
-if [ "$GITHUB_ACTIONS" = "true" ]; then
-    BUILD_DIR=/tmp/build
-    COPY_ARTIFACTS=true
+    # config space
+    cmake /workspaces/embd_event_logging -DCMAKE_BUILD_TYPE=Debug
+
+    # build cmake
+    cmake --build . -- -j$(nproc)
+
+    # build docs
+    cmake --build . --target docs
+
+    # Run test
+    cd tests/
+    ctest --output-on-failure
+    cd -
+
+    if [ "$COPY_ARTIFACTS" = "true" ]; then
+        sudo cp -r /tmp/build/docs_output  /workspaces/artifacts/
+        echo "Copied Artifacts Done"
+    fi
+
+    cd $EXEC_DIR
+}
+
+function example_build()
+{
+    rm -rf ${BUILD_DIR}/example
+    mkdir -p ${BUILD_DIR}/example
+    cd ${BUILD_DIR}/example
+
+    # config space
+    cmake /workspaces/embd_event_logging/example
+
+    # build cmake
+    cmake --build . -- -j$(nproc)
+
+    # execute example
+    ./example
+
+    # create trace analysis directory
+    mkdir -p traces
+    cp generated/metadata traces/
+    cp stream.bin traces/
+
+    # Analyse traces
+    babeltrace2 traces
+
+    cd $EXEC_DIR
+}
+
+# Process named arguments
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        --build-dir)
+            BUILD_DIR="$2"
+            shift 2 # Move past the --build-dir and its value
+            ;;
+        --example)
+            EXAMPLE_BUILD="true"
+            shift 1 # Move past --example
+            ;;
+        --artifacts)
+            COPY_ARTIFACTS="true"
+            shift 1 # Move past --artifacts
+            ;;
+        -h|--help)
+            echo "Usage: $0 [--build-dir <path>] [--example] [--artifacts]"
+            exit 0
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+if [ "$EXAMPLE_BUILD" = "true" ]; then
+    example_build
+else
+    library_ut_test
 fi
 
-# clean build directory
-rm -rf ${BUILD_DIR}
-mkdir ${BUILD_DIR}
-cd ${BUILD_DIR}
-
-# config space
-cmake /workspaces/embd_event_logging -DCMAKE_BUILD_TYPE=Debug
-
-# build cmake
-cmake --build . -- -j$(nproc)
-
-# build docs
-cmake --build . --target docs
-
-# Run test
-cd tests/
-ctest --output-on-failure
-cd -
-
-if [ "$COPY_ARTIFACTS" = "true" ]; then
-    sudo cp -r /tmp/build/docs_output  /workspaces/artifacts/
-    echo "Copied Artifacts Done"
-fi
-
-cd $EXEC_DIR
-
-./example/build_example.sh
+echo "done"
