9522cee799ee438b5cfa7f0c838dbca3c3168f8b
Rohit Patil <rohit.patil.wrc.555@gmail.com>
2025-10-29 16:48:35 +0000

    Title: fix project name issue in CI Type: Fix Component: CI Detail:     - CI take project name from environment variable.

--- repo_setup.sh
+++ repo_setup.sh
@@ -5,4 +5,6 @@
 rm -rf .git/hooks/*
 
 pre-commit install --install-hooks
+pre-commit install -t commit-msg
+git config --local commit.template .github/commit-template.txt
 echo "✅ Git hooks installed using pre-commit"
